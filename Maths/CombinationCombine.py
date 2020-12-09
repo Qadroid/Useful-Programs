8499fc398f9487e6b61076b4ef3b386ecd69527f
Qadroid <34576771+Qadroid@users.noreply.github.com>
2020-12-09 10:09:46 +0000

    Changed code to be able to combine multiple combinations and to respect the order of operation. Also kept a backup of old code

--- Maths/CombinationCombine.py
+++ Maths/CombinationCombine.py
@@ -11,23 +11,58 @@ def findCombination(n, r):
     result = newn / newr
     return result
 
-inputItemCount = int(input("Insert number of items here: "))
-
-## Currently I am going to use a max of 2 till I figure out how to combine and/or
-
-inputRelation = input("Insert relation between the two combinations (and / or): ")
-if inputRelation == "and":
-    for i in range(2):
+inputItemCount = int(input("Insert number of combinations to combine here: "))
+for i in range(inputItemCount):
+        print("\n Input variables for", i+1, "here:")
         inputn = int(input("Insert n variable here: "))
         inputr = int(input("Insert r variable here: "))
         globals()[i] = findCombination(inputn, inputr)
-    print(int(globals()[0] * globals()[1]))
-elif inputRelation == "or":
-    for i in range(2):
-        print("Insert variables for",i+1,"here:")
-        inputn = int(input("Insert n variable here: "))
-        inputr = int(input("Insert r variable here: "))
-        globals()[i] = findCombination(inputn, inputr)
-    print(int(globals()[0] + globals()[1]))
-else:
-    print("Error: Incorrect input")
+
+for p in range(inputItemCount - 1):
+    print("\n Relation between no. of combinations", p+1, "and", p+2)
+    globals()[str(p)+"inputRelation"] = input("Insert relation between the two combinations (and / or): ")
+
+
+# The following code works as long as long as addition is priotized as long as it precedes multiplication
+
+# total = globals()[0]
+
+# for q in range(inputItemCount - 1):
+#     if globals()[str(q)+"inputRelation"] == "and":
+#         total = total * globals()[q+1]
+#     elif globals()[str(q)+"inputRelation"] == "or":
+#         total = total + globals()[q+1]
+
+
+# The following code respects the order of operation
+
+total = globals()[0]
+
+for q in range(inputItemCount - 1):
+    if globals()[str(q)+"inputRelation"] == "and":
+        total = total * globals()[q+1]
+
+for q in range(inputItemCount - 1):
+    if globals()[str(q)+"inputRelation"] == "or":
+        total = total + globals()[q+1]
+
+print("\n Your total is: ", total)
+
+
+# Backup of old code
+
+# if inputRelation == "and":
+#     for i in range(2):
+#         inputn = int(input("Insert n variable here: "))
+#         inputr = int(input("Insert r variable here: "))
+#         globals()[i] = findCombination(inputn, inputr)
+#     print(int(globals()[0] * globals()[1]))
+# elif inputRelation == "or":
+#     for i in range(2):
+#         print("Insert variables for",i+1,"here:")
+#         inputn = int(input("Insert n variable here: "))
+#         inputr = int(input("Insert r variable here: "))
+#         globals()[i] = findCombination(inputn, inputr)
+#     print(int(globals()[0] + globals()[1]))
+# else:
+#     print("Error: Incorrect input")
